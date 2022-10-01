7a36b5d39348bc23d3969cb6c31b77b3da3877c3
bhardwajdisha <disha2.bhardwaj@gmail.com>
2022-10-01 17:07:51 +0530

    created solidity contract for events management

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: '0.8.17',
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `${process.env.MUMBAI_URL}`,
+      accounts: [`${process.env.Metamask}`],
+    },
+  },
 };
